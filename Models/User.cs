f97954e39891fe1c72c39ab2e40df00a07e938eb
WinterP <winterp.dev@gmail.com>
2021-12-06 02:29:39 -0600

    fixed

--- Models/User.cs
+++ Models/User.cs
@@ -9,11 +9,13 @@ namespace dojoSurvey.Models
     public class User
     {
         [Required]
+        [MinLength(2)]
         public string Name {get;set;}
         [Required]
         public string Location {get;set;}
         [Required]
         public string Language {get;set;}
+        [MinLength(20)]
         public string Comment {get;set;}
 
     }
